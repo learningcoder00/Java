9ca2888862f8c1e83a750c7b1a665deaab52fed4
Wang Zehua <3238928273@qq.com>
2025-12-26 14:56:46 +0800

    feat: 将左侧导航栏改为顶部导航栏

--- HROS-UI/src/views/Home.vue
+++ HROS-UI/src/views/Home.vue
@@ -3,65 +3,59 @@
 
     <el-container>
       <el-header class="homeHeader">
-        <div><i class="fa fa-magnet fa-2x"
-                @click="isCollapse = !isCollapse"
-                style="color: #d9c9c9"/>&nbsp; <font id="title">智慧人事管理系统</font>
-        </div>
-
-        <div style="display: flex; align-items: center; margin-right: 7px">
-
-          <span class="el-dropdown-link">
-
-            <i class="fa fa-envelope fa-1x "
-               style="cursor: pointer;margin-right:10px"
-               @click="pageChar"/>
-
-          </span>
-
-          <el-dropdown class="userInfo"
-                       @command="commandHandler">
-            <span class="el-dropdown-link">
-              {{ user.name }}
-              <i> <img :src="user.userface"></i>
-            </span>
-            <el-dropdown-menu slot="dropdown">
-              <el-dropdown-item command="logout"
-                                divided>注销登录
-              </el-dropdown-item>
-            </el-dropdown-menu>
-          </el-dropdown>
-        </div>
-      </el-header>
-      <el-container>
-        <el-aside class="home-aside"
-                  :style="{width:leftclass}">
-          <el-scrollbar style="height: 100%">
-            <el-menu class="el-menu-vertical-demo"
+        <div style="display: flex; align-items: center; width: 100%">
+          <div style="margin-right: 30px"><i class="fa fa-magnet fa-2x" style="color: #d9c9c9"/>&nbsp; <font id="title">智慧人事管理系统</font></div>
+          
+          <!-- 水平导航菜单 -->
+          <div style="flex: 1">
+            <el-menu class="el-menu-horizontal"
                      router
                      unique-opened
                      active-text-color="#67C23A"
-                     :collapse="isCollapse"
+                     mode="horizontal"
                      @select="menusClick">
               <el-submenu :index="index+''"
                           v-for="(item, index) in routes"
                           :key="index">
                 <template slot="title">
-                  <i style="color: #409eff; margin-right: 15px;"
+                  <i style="color: #ffffff; margin-right: 5px;"
                      :class="item.iconcls"></i>
-                  <span slot="title">{{ item.name }}</span>
+                  <span>{{ item.name }}</span>
                 </template>
                 <el-menu-item :index="child.path"
                               v-for="( child, indexj ) in item.children"
                               :key="indexj">
-                  <i style="color: #409eff; margin:0 15px;"
+                  <i style="color: #409eff; margin:0 5px;"
                      :class="child.iconcls"></i>
-                  <span slot="title">{{ child.name }}</span>
+                  <span>{{ child.name }}</span>
                 </el-menu-item>
               </el-submenu>
             </el-menu>
-          </el-scrollbar>
-        </el-aside>
-        <el-main>
+          </div>
+
+          <div style="display: flex; align-items: center; margin-left: 30px">
+            <span class="el-dropdown-link">
+              <i class="fa fa-envelope fa-1x "
+                 style="cursor: pointer;margin-right:10px"
+                 @click="pageChar"/>
+            </span>
+
+            <el-dropdown class="userInfo"
+                         @command="commandHandler">
+              <span class="el-dropdown-link">
+                {{ user.name }}
+                <i> <img :src="user.userface"></i>
+              </span>
+              <el-dropdown-menu slot="dropdown">
+                <el-dropdown-item command="logout"
+                                  divided>注销登录
+                </el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+          </div>
+        </div>
+      </el-header>
+      <el-main>
           <el-breadcrumb separator-class="el-icon-arrow-right">
             <el-breadcrumb-item :to="{ path: '/home' }"
                                 v-show=" this.$router.currentRoute.path!=='/home'">
@@ -70,14 +64,13 @@
             <!--获取当前路由对象的指定属性-->
             <el-breadcrumb-item>{{ this.$router.currentRoute.name }}</el-breadcrumb-item>
           </el-breadcrumb>
-          <div :class="{homeRouterViewA: isa, homeRouterViewB: isb}"
+          <div class="homeRouterViewA"
                v-if="this.$router.currentRoute.path=='/home'">
 
             <Main/>
           </div>
-          <router-view :class="{homeRouterViewA: isa, homeRouterViewB: isb}"/>
+          <router-view class="homeRouterViewA"/>
         </el-main>
-      </el-container>
     </el-container>
     <el-dialog :visible.sync="dialogTableVisible"
                width="0%"
@@ -98,14 +91,6 @@ export default {
     return {
       //获取用户信息，从sessionStorage中提取信息转换为
       user: JSON.parse(window.sessionStorage.getItem("user")),
-      // 菜单栏的显示方式
-      isCollapse: false,
-      /*条*/
-      loading: false,
-      leftclass: '205px',
-      rightclass: '1325px',
-      isa: true,
-      isb: false,
       dialogTableVisible: false,
     }
   },
@@ -125,8 +110,7 @@ export default {
     },
     /*子菜单的单机触发事件*/
     menusClick() {
-      this.isCollapse = false;
-
+      // 顶部导航栏不需要折叠功能
     },
     /*注销*/
     commandHandler(cmd) {
@@ -164,19 +148,7 @@ export default {
 
   },
   watch: {
-    isCollapse: function (value) {
-      if (value) {
-        this.leftclass = '50px';
-        this.rightclass = "1400px";
-        this.isa = false;
-        this.isb = true;
-      } else {
-        this.leftclass = '205px';
-        this.rightclass = "1295px";
-        this.isa = true;
-        this.isb = false;
-      }
-    }
+    // 顶部导航栏不需要监听折叠状态
   },
   components: {
     Main, pageChar
@@ -193,40 +165,15 @@ export default {
   justify-content: center;
 }
 
-.home-aside .el-scrollbar__wrap {
-  overflow-x: hidden;
-}
-
-/* .homeWelcome {
-        text-align: center;
-        font-size: 30px;
-        font-family: 站酷庆科黄油体;
-        color: #409eff;
-        display: flex;
-        !*横轴排列*!
-        flex-direction:row-reverse;
-        !*项目两端对齐，项目之间的间隔都相等*!
-        justify-content:space-between;
-        align-items: center;
-
-    }
-*/
-.homeRouterViewA {
-  margin-top: 10px;
-  width: 1645px;
-  height: 780px;
-}
-
+.homeRouterViewA,
 .homeRouterViewB {
   margin-top: 10px;
-  width: 1800px;
-  height: 780px;
+  width: 100%;
+  min-height: calc(100vh - 100px);
 }
 
 .homeHeader {
   background-color: #76d3e3;
-  /*background-image: linear-gradient(#409EFF, #FFFFFF);*/
-
   /* //弹性布局,设为Flex布局以后，子元素的float、clear和vertical-align属性将失效。*/
   display: flex;
   /* //居中对齐弹性盒的各项  元素：*/
@@ -239,11 +186,7 @@ export default {
   /*向方框添加一个或多个阴影。*/
   /*box-shadow:0px 10px 10px -4px #409eff;*/
   font-family: 站酷庆科黄油体;
-}
-
-.el-menu-vertical-demo el-menu {
-  border: #ffffff;
-  border-right: olid 0px #ffffff;
+  height: 60px;
 }
 
 #title {
@@ -274,13 +217,16 @@ export default {
   border: #ffffff;
 }
 
-.el-menu-vertical-demo:not(.el-menu--collapse) {
-  width: 200px;
-  min-height: 100%;
-  border: #ffffff;
+.el-menu-horizontal {
+  border-bottom: none;
+  background-color: transparent;
 }
 
-el-menu {
-  border-right: solid 0px #ffffff;
+.el-menu-horizontal .el-menu-item,
+.el-menu-horizontal .el-submenu__title {
+  color: #ffffff;
+  height: 60px;
+  line-height: 60px;
+  border-bottom: none;
 }
 </style>
